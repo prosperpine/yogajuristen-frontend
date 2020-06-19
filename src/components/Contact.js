9b781ccb33f5da6240796eacd9b327d56ca16804
Josephine <josephine@Josephines-Air.lan>
2020-06-19 19:03:51 +0200

    finished About page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com';
 
 export const Contact = () => {
   const [feedback, setFeedback] = useState('')
@@ -50,4 +51,30 @@ export const Contact = () => {
     </form>
   )
 }
+
+//export const Contact = () => {
+  //   const sendEmail = (e) => {
+  //     e.preventDefault();
+
+  //     emailjs.sendForm('gmail', 'template_1YsBvThp', e.target, 'USER_ID')
+  //       .then((result) => {
+  //         console.log(result.text);
+  //       }, (error) => {
+  //         console.log(error.text);
+  //       });
+  //   }
+
+  //   return (
+  //     <form className="contact-form" onSubmit={sendEmail}>
+  //       <input type="hidden" name="contact_number" />
+  //       <label>Name</label>
+  //       <input type="text" name="user_name" />
+  //       <label>Email</label>
+  //       <input type="email" name="user_email" />
+  //       <label>Message</label>
+  //       <textarea name="message" />
+  //       <input type="submit" value="Send" />
+  //     </form>
+  //   );
+  // }
 
