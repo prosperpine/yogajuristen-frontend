6680ce8615041acb20ed6dfd3e6ed9c2df61bb16
Josephine <josephine@Josephines-Air.lan>
2020-06-21 10:31:34 +0200

    changed toml file and folders

---
+++ code/build/precache-manifest.6d011186bd93595aef4d23ebac02a728.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbc1407842958685603758a4100af81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f95e475ea885cb1069",
+    "url": "/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "1d375d4dd41ebf9daac5",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "57f95e475ea885cb1069",
+    "url": "/static/js/2.e64db528.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.e64db528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d375d4dd41ebf9daac5",
+    "url": "/static/js/main.da5227f6.chunk.js"
+  },
+  {
+    "revision": "2a88c966eaaa02696f0a",
+    "url": "/static/js/runtime-main.1b4414a0.js"
+  },
+  {
+    "revision": "50ac7db71d21370108e31aecfc91863e",
+    "url": "/static/media/RYT.50ac7db7.png"
+  },
+  {
+    "revision": "fa95d7176d22b3a0e32a1a37750ccf73",
+    "url": "/static/media/lake.fa95d717.jpg"
+  },
+  {
+    "revision": "bab6817587e467f4123d14aecfc54a2e",
+    "url": "/static/media/mountains.bab68175.jpg"
+  },
+  {
+    "revision": "6253097aae178d2faf2803691189e4a4",
+    "url": "/static/media/pranayama.6253097a.mp4"
+  },
+  {
+    "revision": "6ef118a26db4f70a2f58802b5a3cb115",
+    "url": "/static/media/stones.6ef118a2.jpg"
+  },
+  {
+    "revision": "ab0387b416fb9e1b31c7d36a380efffb",
+    "url": "/static/media/video.ab0387b4.mp4"
+  },
+  {
+    "revision": "26e2e0618c81194d0655c0e5b6e78a06",
+    "url": "/static/media/warrior.26e2e061.jpg"
+  },
+  {
+    "revision": "db0fe470d6d0525f3d6ecd12021c4c64",
+    "url": "/static/media/yogamat.db0fe470.jpg"
+  },
+  {
+    "revision": "189740edb6b473c2100c11e131504c70",
+    "url": "/static/media/yogayama.189740ed.png"
+  }
+]);
